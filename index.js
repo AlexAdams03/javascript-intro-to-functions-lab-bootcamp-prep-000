067f65c120ab9019a2c3fea73b78a35ba7f651f4
alexadams0322@yahoo.com <alexadams0322@yahoo.com>
2018-05-24 14:04:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function shout(string) {
 
 function whisper(string) {
   return string.toLowerCase()
+<<<<<<< HEAD
 }
 
 var uppercase = "HELLO!"
@@ -19,3 +20,6 @@ var mixedCase = 'Hi there!'
 mixedCase.toLowerCase() === mixedCase // false
  
 mixedCase.toUpperCase() === mixedCase // false
+=======
+}
+>>>>>>> 924c5462b362d8f7135e3fe51e9af59925d18fe3
